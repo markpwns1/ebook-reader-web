9470d010771dcdb8c5c666cb538b7f74b7ea9886
Mark <mark_kaldas@yahoo.com>
2021-05-31 11:40:12 -0600

    added links, svg image support, and dynamic loading

--- index.js
+++ index.js
@@ -5,11 +5,103 @@ const book = {
     spine: [ ]
 }
 
+let $content;
+let currentPage = 0;
+let loaded = [ ];
+let loading = false;
+
+const loadPage = (index, callback) => {
+
+    if(loading) return;
+    if(index < currentPage) return;
+    if(index >= book.spine.length) return;
+    
+    currentPage = index;
+    loading = true;
+
+    const content = book.content.find(x => x.id == book.spine[index]);
+
+    book.zip.file(content.href).async("text").then(txt => {
+        loaded.push(content.href);
+        const page = $(txt);
+
+        for(const img of page.find("img")) {
+            const imgSrc = img.getAttribute("src");
+            book.zip.file(imgSrc).async("blob").then(blob => {
+                img.src = URL.createObjectURL(blob);
+            });
+            img.classList.add("centered-image");
+        }
+
+        for (const img of page.find("image")) {
+            const $newImage = $(img);
+            book.zip.file($newImage.attr("xlink:href")).async("blob").then(blob => {
+                $newImage.attr("xlink:href", URL.createObjectURL(blob));
+            });
+        }
+
+        for (const a of page.find("a")) {
+            const $a = $(a);
+            const link = $a.attr("href");
+            $a.removeAttr("href");
+            $a.click(() => {
+                // console.log(link.substring(0, link.indexOf("#")));
+                // console.log(book.spine);
+                const foundContent = book.content.find(x => x.href == link.substring(0, link.indexOf("#")));
+                const foundSpineEntry = book.spine.find(x => x == foundContent.id);
+                const index = book.spine.indexOf(foundSpineEntry);
+
+                const onReady = () => {
+                    // if(!link.includes("#"))
+                    const url = location.href;
+                    location.href = link.substring(link.indexOf("#"));
+                    history.replaceState(null, null, url);   
+                }
+
+                const loadLoop = () => {
+                    if(currentPage < index)
+                        loadPage(currentPage + 1, loadLoop);
+                    else 
+                        onReady();
+                }
+
+                if(index <= currentPage) {
+                    onReady();
+                }
+                else {
+                    loadLoop();
+                }
+                // console.log(loaded);
+            });
+        }
+
+        $content.append(page);
+        loading = false;
+
+        if(callback) callback();
+        // loadPage(index + 1);
+    });
+}
+
 window.onload = () => {
-    var $result = $("#result");
+
+    $content = $("#result");
+
+    
+
+    $("#btn-load-more").click(() => {
+        loadPage(currentPage + 1);
+    });
+
+    window.onscroll = function(ev) {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            loadPage(currentPage + 1);
+        }
+    };
+
     $("#file").on("change", function(evt) {
         // remove content
-        $result.html("");
+        $content.html("");
 
         // Closure to capture the file information.
         function handleFile(f) {
@@ -44,23 +136,7 @@ window.onload = () => {
                     }
 
                     // let i = 0;
-                    const loadPage = index => {
-                        zip.file(book.content.find(x => x.id == book.spine[index]).href).async("text").then(txt => {
-                            const page = $(txt);
-                            for(const img of page.find("img")) {
-                                const imgSrc = img.getAttribute("src");
-                                zip.file(imgSrc).async("blob").then(blob => {
-                                    const blobUrl = URL.createObjectURL(blob) // blob is the Blob object
-                                    img.src = blobUrl;
-                                });
-                                img.classList.add("centered-image");
-                                // console.log()
-                                // console.log(img.attr("src"))
-                            }
-                            $result.append(page);
-                            loadPage(index + 1);
-                        });
-                    }
+                    
                     loadPage(0);
                     
                     // console.log(book)
